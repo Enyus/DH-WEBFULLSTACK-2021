0ddbe7d7f12a0e57108995ad1a425344401064c8
Enyus <enyus@hotmail.com>
2021-11-27 20:37:42 -0300

    teste

--- aula16/dhpet/routes/contato.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-let contatoController = require('../controllers/contatoController');
-
-router.get('/', contatoController.index)
-
-router.post('/cadastrar', contatoController.cadastrar)
-
-module.exports = router
